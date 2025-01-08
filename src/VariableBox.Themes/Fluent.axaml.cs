347ab163cbf29fb53acf32a5d7f46d13199e0caf
heartacker <heartacker@outlook.com>
2025-01-08 16:43:48 +0800

    feat: 添加Fluent和Semi主题支持，包含暗亮模式和本地化资源
    - Add Fluent theme configuration with dark and light mode support, shared resources, and localized styles.
    - Implement a `FluentTheme` class with locale-aware resource loading for Avalonia UI themes.
    - Add a new Semi theme with dark and light mode support, shared resources, and localized content.
    - Implement a theme class with locale-specific resource handling for Avalonia applications.

---
+++ src/VariableBox.Themes/Fluent.axaml.cs
@@ -0,0 +1,71 @@
+using System.Globalization;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using Avalonia.Styling;
+using VariableBox.Avalonia.Themes.Locale;
+
+namespace VariableBox.Avalonia.Themes;
+
+/// <summary>
+/// Notice: Don't set Locale if your app is in InvariantGlobalization mode.
+/// </summary>
+public class FluentTheme: Styles
+{
+    private static readonly Lazy<Dictionary<CultureInfo, ResourceDictionary>> _localeToResource 
+        = new Lazy<Dictionary<CultureInfo, ResourceDictionary>>(
+        () => new Dictionary<CultureInfo, ResourceDictionary>
+        {
+            { new CultureInfo("zh-CN"), new zh_cn() },
+            { new CultureInfo("en-US"), new en_us() },
+        });
+
+    private static readonly ResourceDictionary _defaultResource = new zh_cn();
+
+    private readonly IServiceProvider? sp;
+    public FluentTheme(IServiceProvider? provider = null)
+    {
+        sp = provider;
+        AvaloniaXamlLoader.Load(provider, this);
+    }
+
+    private CultureInfo? _locale;
+    public CultureInfo? Locale
+    {
+        get => _locale;
+        set
+        {
+            try
+            {
+                _locale = value;
+                var resource = TryGetLocaleResource(value);
+                if (resource is null) return;
+                foreach (var kv in resource)
+                {
+                    this.Resources[kv.Key] = kv.Value;
+                }
+            }
+            catch
+            {
+                _locale = CultureInfo.InvariantCulture;
+            }
+
+        }
+    }
+
+    private static ResourceDictionary? TryGetLocaleResource(CultureInfo? locale)
+    {
+        if (Equals(locale, CultureInfo.InvariantCulture))
+        {
+            return _defaultResource;
+        }
+        if (locale is null)
+        {
+            return _localeToResource.Value[new CultureInfo("zh-CN")];
+        }
+        if (_localeToResource.Value.TryGetValue(locale, out var resource))
+        {
+            return resource;
+        }
+        return _localeToResource.Value[new CultureInfo("zh-CN")];
+    }
+}
