3e8fe282121acbecc4872dd9bf1e58185813d108
heartacker <heartacker@outlook.com>
2025-01-09 22:50:09 +0800

    添加主题切换功能，支持Fluent、Simple和Semi主题

--- demo/Ursa.Demo/ViewModels/MainWindowViewModel.cs
+++ demo/Ursa.Demo/ViewModels/MainWindowViewModel.cs
@@ -1,5 +1,8 @@
 ﻿using Avalonia;
+using Avalonia.Markup.Xaml.Styling;
 using Avalonia.Styling;
+using Avalonia.Themes.Fluent;
+using Avalonia.Themes.Simple;
 using CommunityToolkit.Mvvm.Input;
 
 namespace VariableBox.Demo.ViewModels;
@@ -12,8 +15,42 @@ public partial class MainWindowViewModel : ViewModelBase
     private void ToggleTheme()
     {
         var app = Application.Current;
-        if (app is null) return;
+        if (app is null)
+            return;
         var theme = app.ActualThemeVariant;
-        app.RequestedThemeVariant = theme == ThemeVariant.Dark ? ThemeVariant.Light : ThemeVariant.Dark;
+        app.RequestedThemeVariant =
+            theme == ThemeVariant.Dark ? ThemeVariant.Light : ThemeVariant.Dark;
+    }
+
+    [RelayCommand]
+    private void SwitchTheme(string themeName)
+    {
+        var app = Application.Current;
+        if (app is null)
+            return;
+
+        Styles st = Application.Current.Styles[0] as Styles;
+        app.Styles.Clear();
+
+        switch (themeName)
+        {
+            case "Fluent":
+                st = new FluentTheme();
+                app.Styles.Add(st);
+                app.Styles.Add(new VariableBox.Themes.FluentTheme());
+                break;
+            case "Simple":
+                st = new SimpleTheme();
+                app.Styles.Add(st);
+                app.Styles.Add(new VariableBox.Themes.SimpleTheme());
+                break;
+            case "Semi":
+                st = new Semi.Avalonia.SemiTheme();
+                app.Styles.Add(st);
+                app.Styles.Add(new VariableBox.Themes.SemiTheme());
+                break;
+        }
+
+        Application.Current.Styles[0] = st;
     }
 }
