ae866d058f7fd33c2d798c76f1ae32e79474b705
heartacker <heartacker@outlook.com>
2025-02-20 22:38:34 +0800

    feat: 添加 ManyVariableBox 用户控件及其 XAML 设计

---
+++ demo/VariableBox.Demo/Pages/ManyVariableBox.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace VariableBox.Demo.Pages;
+
+public partial class ManyVariableBox : UserControl
+{
+    public ManyVariableBox()
+    {
+        InitializeComponent();
+    }
+}
