a0ed1b6948d5366ce4e73f7332320fe337630ac2
heartacker <heartacker@outlook.com>
2025-03-10 16:50:02 +0800

    demo

--- demo/VariableBox.Demo/ViewModels/DemoMessageValue.cs
+++ demo/VariableBox.Demo/ViewModels/DemoMessageValue.cs
@@ -5,6 +5,16 @@ using CommunityToolkit.Mvvm.Messaging.Messages;
 namespace VariableBox.Demo.ViewModels;
 
 // Create a message
+/// <summary>
+/// 只要注册了这个消息的接收者，就会收到消息。
+/// 参考：
+/// <br/>
+/// <see cref="CommunityToolkit.Mvvm.Messaging.Messages.ValueChangedMessage"/>
+/// <br/>
+/// <see cref="CommunityToolkit.Mvvm.Messaging.Messages.PropertyChangedMessage"/>
+/// <br/>
+/// <see cref="CommunityToolkit.Mvvm.Messaging.Messages.RequestMessage"/>
+/// </summary>
 public class UIntValueChangedMessage : ValueChangedMessage<UInt32>
 {
     public UIntValueChangedMessage(UInt32 v) : base(v)
